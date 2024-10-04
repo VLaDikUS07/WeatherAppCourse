e0fc3b701e5274f0e955ed1cc90462df5c305300
VLaDikUS <mrvladikus@yandex.ru>
2024-10-04 17:59:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/weatherappcourse/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.weatherappcourse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherappcourse.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
+    }
+}
